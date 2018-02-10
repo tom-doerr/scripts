c94d8810f3347fd201a57a123435cc2c5870fd59
esteer <tomdoerr96@gmail.com>
2018-02-10 19:50:49 +0100

    Add files for connecting to desktop pc over netcup

---
+++ netcup/connect_desktop.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+gnome-terminal -x bash --norc -c 'ssh v22016124111441558.luckysrv.de -p 8365'
+exit
---
+++ netcup/start_forwarding_desktop.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+ssh -nNT -R 8365:localhost:22 root@v22016124111441558.luckysrv.de
