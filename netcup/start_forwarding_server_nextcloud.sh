d211b229c5116ffb79023eba8bcc50bdf7b4a80d
Tom Doerr <tomdoerr96@gmail.com>
2018-02-15 02:02:08 +0100

    Add timestamp to output

--- netcup/start_forwarding_server_nextcloud.sh
+++ netcup/start_forwarding_server_nextcloud.sh
@@ -2,7 +2,7 @@
 
 while true
 do
-    echo Connecting
+    echo `date --rfc-3339=seconds`   Connecting
     ssh -o TCPKeepAlive=no -o ServerAliveInterval=15 -nNT -R 444:localhost:443 root@v22016124111441558.luckysrv.de
     sleep 1
 done
