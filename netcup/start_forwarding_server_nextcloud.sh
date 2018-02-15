6a89139c30ac82b54a6238f8a128bacd7048b32c
Tom Doerr <tomdoerr96@gmail.com>
2018-02-15 01:57:09 +0100

    Change keepAlive option; Add restart loop

--- netcup/start_forwarding_server_nextcloud.sh
+++ netcup/start_forwarding_server_nextcloud.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-ssh -nNT -R 443:localhost:443 root@v22016124111441558.luckysrv.de
+while true
+do
+    ssh -o TCPKeepAlive=no -o ServerAliveInterval=15 -nNT -R 444:localhost:443 root@v22016124111441558.luckysrv.de
+done
