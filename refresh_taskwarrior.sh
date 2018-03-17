74d958d8deac72b6a37dafde1ec278650c907c8b
tom <tom.doerr@tum.de>
2018-03-17 17:17:03 +0100

    Add refresh script

---
+++ refresh_taskwarrior.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+filename=~/Nextcloud/sonstiges/timewarrior/data/
+
+inotifywait -r -q -m -e close_write $filename |
+while read -r filename event; do
+    clear
+    task # or "./$filename"
+done
