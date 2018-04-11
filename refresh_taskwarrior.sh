69e93940885185f39f35fff361fba0ec011418c3
esteer <tomdoerr96@gmail.com>
2018-04-11 20:58:16 +0200

    Add path for configuration

--- refresh_taskwarrior.sh
+++ refresh_taskwarrior.sh
@@ -1,12 +1,1 @@
-#! /bin/bash
-
-filename=~/Nextcloud/sonstiges/task/backlog.data
-
-clear
-task
-
-inotifywait -r -q -m -e close_write $filename |
-while read -r filename event; do
-    clear
-    task # or "./$filename"
-done
+./task/refresh_tasks_inotifywatch.sh
