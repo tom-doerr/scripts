87f924727ebfdd2942fa7686b64b3391d9e9f1a5
Tom Doerr <tomdoerr96@gmail.com>
2018-09-02 07:53:59 +0200

    Add script

---
+++ task/save_projects_to_list.py
@@ -0,0 +1,22 @@
+import subprocess
+PROCESS = subprocess.Popen('task _projects'.split(), stdout=subprocess.PIPE)
+OUTPUT, ERROR = PROCESS.communicate()
+projects_list = set()
+for s in OUTPUT.decode('UTF-8').split('\n'):
+    s = s.replace('\n', '')
+    projects_list.add(s)
+
+
+with open('/home/tom/Nextcloud/documents/gtd/projects', 'r') as f:
+    content = f.readlines()
+    
+for s in content:
+    s = s.replace('\n', '')
+    projects_list.add(s)
+
+print(projects_list)
+
+
+with open('/home/tom/Nextcloud/documents/gtd/projects', 'w') as f:
+    for e in sorted(list(projects_list)):
+        f.write(e + '\n')
