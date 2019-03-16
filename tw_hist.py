89c91e01500de47fea1276e72302ca4d7ed1b2a7
Tom Doerr <tomdoerr96@gmail.com>
2019-03-16 10:46:25 +0100

    Refactoring

--- tw_hist.py
+++ tw_hist.py
@@ -5,31 +5,36 @@ import re
 import sys
 from bashplotlib.histogram import plot_hist
 
-try:
-    tracking_label=sys.argv[1:]
-except:
-    tracking_label=list()
 
-result = subprocess.run(['timew', 'su', '2018-01-01', '-', 'tomorrow']+tracking_label, stdout=subprocess.PIPE)
+def plot_hist_tw(tracking_label):
+    result = subprocess.run(['timew', 'su', '2018-01-01', '-', 'tomorrow']+tracking_label, stdout=subprocess.PIPE)
 
-durations = list()
-for e in str(result.stdout).split('\\n'):
-    print(e)
-    m = re.search(r"(?<=..:..:..)..:..:..", e)
-    if m != None:
-        durations += [m.group(0)]
+    durations = list()
+    for e in str(result.stdout).split('\\n'):
+        print(e)
+        m = re.search(r"(?<=..:..:..)..:..:..", e)
+        if m != None:
+            durations += [m.group(0)]
 
 
-print('\n\n' + str(durations))
-duration_minutes = [int(e.split(':')[0])*60+int(e.split(':')[1]) for e in durations]
-print(duration_minutes)
+    print('\n\n' + str(durations))
+    duration_minutes = [int(e.split(':')[0])*60+int(e.split(':')[1]) for e in durations]
+    print(duration_minutes)
 
-#print(subprocess.run(['bashplotlib']+duration_minutes))
-plot_hist(duration_minutes, bincount=100, xlab=False)
+    #print(subprocess.run(['bashplotlib']+duration_minutes))
+    plot_hist(duration_minutes, bincount=100, xlab=False)
 
-duration_min = min(duration_minutes)
-duration_max = max(duration_minutes)
-bin_number = duration_minutes[-1] /((duration_max - duration_min) / 100)
-print('     ' + ' ' * int(bin_number) + '\033[31;1;4m#\033[0m')
+    duration_min = min(duration_minutes)
+    duration_max = max(duration_minutes)
+    bin_number = duration_minutes[-1] /((duration_max - duration_min) / 100)
+    print('     ' + ' ' * int(bin_number) + '\033[31;1;4m#\033[0m')
 
+
+if __name__ == '__main__':
+    try:
+        tracking_label=sys.argv[1:]
+    except:
+        tracking_label=list()
+
+    plot_hist_tw(tracking_label)
 
