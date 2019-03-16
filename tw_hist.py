68893e6d63da2942f1952a74b3b0dc5a42626984
Tom Doerr <tomdoerr96@gmail.com>
2019-03-16 11:06:28 +0100

    Comment out print statement

--- tw_hist.py
+++ tw_hist.py
@@ -11,23 +11,25 @@ def plot_hist_tw(tracking_label):
 
     durations = list()
     for e in str(result.stdout).split('\\n'):
-        print(e)
+        #print(e)
         m = re.search(r"(?<=..:..:..)..:..:..", e)
         if m != None:
             durations += [m.group(0)]
 
 
-    print('\n\n' + str(durations))
+    #print('\n\n' + str(durations))
     duration_minutes = [int(e.split(':')[0])*60+int(e.split(':')[1]) for e in durations]
-    print(duration_minutes)
+    #print(duration_minutes)
 
     #print(subprocess.run(['bashplotlib']+duration_minutes))
     plot_hist(duration_minutes, bincount=100, xlab=False)
 
     duration_min = min(duration_minutes)
     duration_max = max(duration_minutes)
-    bin_number = duration_minutes[-1] /((duration_max - duration_min) / 100)
-    print('     ' + ' ' * int(bin_number) + '\033[31;1;4m#\033[0m')
+    bin_size = (duration_max - duration_min) / 100
+    if bin_size != 0:
+        bin_number = duration_minutes[-1] / bin_size
+        print('     ' + ' ' * int(bin_number) + '\033[31;1;4m#\033[0m')
 
 
 if __name__ == '__main__':
