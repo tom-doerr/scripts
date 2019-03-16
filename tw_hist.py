82ec83afe3671d126167d0b25d7615b0d3b13a18
Tom Doerr <tomdoerr96@gmail.com>
2019-03-16 10:39:34 +0100

    Add color marker

--- tw_hist.py
+++ tw_hist.py
@@ -25,5 +25,11 @@ duration_minutes = [int(e.split(':')[0])*60+int(e.split(':')[1]) for e in durati
 print(duration_minutes)
 
 #print(subprocess.run(['bashplotlib']+duration_minutes))
-plot_hist(duration_minutes, bincount=100, xlab=True)
+plot_hist(duration_minutes, bincount=100, xlab=False)
+
+duration_min = min(duration_minutes)
+duration_max = max(duration_minutes)
+bin_number = duration_minutes[-1] /((duration_max - duration_min) / 100)
+print('     ' + ' ' * int(bin_number) + '\033[31;1;4m#\033[0m')
+
 
