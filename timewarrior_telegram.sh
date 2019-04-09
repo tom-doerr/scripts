ccd561e840ac055561063205e2ca85f91b71c381
Tom Doerr <tomdoerr96@gmail.com>
2019-04-09 23:58:23 +0200

    Fix pattern matching

--- timewarrior_telegram.sh
+++ timewarrior_telegram.sh
@@ -3,7 +3,7 @@
 while true; do
     tw_output=$(timew)
     tracking_line=$(printf "$tw_output" | awk 'NR==1')  
-    if [[ $tracking_line == *" video "* ]] && [[ $(printf "$tw_output" | awk 'NR==4 {print $2}') > '0:15:00' ]]; then
+    if [[ $tracking_line == *" video"* ]] && [[ $(printf "$tw_output" | awk 'NR==4 {print $2}') > '0:15:00' ]]; then
         timew | telegram-send --stdin
         sleep 900
     fi
