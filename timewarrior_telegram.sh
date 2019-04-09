1fd9f3ebab9b3a58216c9d0fb0f0f2d2edc8549f
Tom Doerr <tomdoerr96@gmail.com>
2019-04-09 23:19:24 +0200

    Remove unnecessary variables

--- timewarrior_telegram.sh
+++ timewarrior_telegram.sh
@@ -2,8 +2,6 @@
 
 while true; do
     tw_output=$(timew)
-    t1=$(printf "$tw_output" | awk 'NR==1 {print $2}')
-    t2=$(printf "$tw_output" | awk 'NR==1 {print $3}')
     tracking_line=$(printf "$tw_output" | awk 'NR==1')  
     if [[ $tracking_line == *" video "* ]] && [[ $(printf "$tw_output" | awk 'NR==4 {print $2}') > '0:15:00' ]]; then
         timew | telegram-send --stdin
