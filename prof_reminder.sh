e808337c35355afebf84a7226191f96bdb7c2eac
Tom Doerr <tomdoerr96@gmail.com>
2019-08-09 16:02:50 +0200

    Check whether sleeping

--- prof_reminder.sh
+++ prof_reminder.sh
@@ -5,6 +5,7 @@ END_TIME=1800
 TELEGRAM_MESSAGE_TO_SEND='prof_reminder: Break lasts for more than 15mins'
 SLEEP_TIME_BETWEEN_MESSAGES=60
 SLEEP_BETWEEN_CHECKS=10
+NO_DATA_TEXT="No filtered data found in the range"
 
 
 
@@ -14,8 +15,10 @@ do
     time_without_leading_zero=${time_with_leading_zero#0}
     if (( $time_without_leading_zero > $START_TIME )) && (( $time_without_leading_zero < $END_TIME ))
     then
-        timew_output=$(timew su $(date --date "15 minutes ago" +%H%M) - tomorrow prof | tail -2 | head -1)
-        if [[ $timew_output == *"No filtered data found in the range"* ]]
+        timew_output_prof=$(timew su $(date --date "15 minutes ago" +%H%M) - tomorrow prof | tail -2 | head -1)
+        timew_output_schlafen=$(timew su $(date --date "15 minutes ago" +%H%M) - tomorrow schlafen | tail -2 | head -1)
+
+        if [[ $timew_output_prof == *$NO_DATA_TEXT* ]] && [[ $timew_output_schlafen == *$NO_DATA_TEXT* ]]
         then
             telegram-send "$TELEGRAM_MESSAGE_TO_SEND"
             sleep $SLEEP_TIME_BETWEEN_MESSAGES
