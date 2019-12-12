39443e412ed9b8705d20826f0ad801f18a82dc16
Tom Doerr <tomdoerr96@gmail.com>
2019-12-12 01:32:20 +0100

    Add skript

---
+++ telegram_video_prof.sh
@@ -0,0 +1,36 @@
+#!/bin/zsh
+
+source ~/.zshrc
+
+get_time_h_day() {
+    hms_to_hours $(timew su $1 | tail -2 | head -1 | { read first rest; echo $first; })
+}
+
+currently_tracking_video() {
+    tw_output=$(timew)
+    tracking_line=$(printf "$tw_output" | awk 'NR==1')  
+    [[ $tracking_line == *" video"* ]] 
+}
+
+to_much_video() {
+    h_prof=$(get_time_h_day prof)
+    h_video=$(get_time_h_day video)
+    [[ $(($h_video * 4)) > $h_prof ]] 
+}
+
+while true
+do
+    if currently_tracking_video
+    then
+        if to_much_video
+        then
+            h_prof=$(get_time_h_day prof)
+            h_prof_rounded=$(printf "%.2f\n" $h_prof)
+            h_video=$(get_time_h_day video)
+            h_video_rounded=$(printf "%.2f\n" $h_video)
+            echo "Spent too much time watching videos ($h_video_rounded) vs working on prof tasks ($h_prof_rounded)" | telegram-send --stdin
+        fi
+
+    fi
+    sleep 2
+done
