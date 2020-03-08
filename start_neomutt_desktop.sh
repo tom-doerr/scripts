7f6d2a3e44d784571b816f1f3a8d3b3934573392
Tom Doerr <tomdoerr96@gmail.com>
2020-03-08 09:04:20 +0100

    Update command desktop

--- start_neomutt_desktop.sh
+++ start_neomutt_desktop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-if [[ $(uname --nodename) == 'tom-Desktop-18' ]]; then kitty zsh; else kitty ssh -X -t gard neomutt; fi 
+if [[ $(uname --nodename) == 'tom-Desktop-18' ]]; then kitty neomutt; else kitty ssh -X -t gard neomutt; fi 
