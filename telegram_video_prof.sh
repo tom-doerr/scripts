5f3940d21a0429f1214781748c0c1af3f338e929
Tom Dörr <tom.doerr@tum.de>
2020-08-13 20:49:59 +0200

    Fix bug

--- telegram_video_prof.sh
+++ telegram_video_prof.sh
@@ -2,10 +2,11 @@
 
 source ~/.zshrc
 
-currently_tracking_video_or_break() {
+currently_tracking_tag() {
+    tag="$1"
     tw_output=$(timew)
     tracking_line=$(printf "$tw_output" | awk 'NR==1')  
-    [[ $tracking_line =~ " video| break" ]] 
+    [[ $tracking_line =~ " $tag" ]] 
 }
 
 too_much_video() {
@@ -26,20 +27,17 @@ too_much_tag_time() {
 
 while true
 do
-    if currently_tracking_video_or_break
+    if too_much_video && currently_tracking_tag "video"
     then
-        if too_much_video
-        then
-            h_prof=$(get_time_h_day prof)
-            h_prof_rounded=$(printf "%.2f\n" $h_prof)
-            h_video=$(get_time_h_day video)
-            h_video_rounded=$(printf "%.2f\n" $h_video)
-            echo "Spent too much time watching videos ($h_video_rounded) vs working on prof tasks ($h_prof_rounded)" | telegram-send --stdin
-        fi
-        if too_much_break
-        then
-            echo "Too much break time!" | telegram-send --stdin
-        fi
+        h_prof=$(get_time_h_day prof)
+        h_prof_rounded=$(printf "%.2f\n" $h_prof)
+        h_video=$(get_time_h_day video)
+        h_video_rounded=$(printf "%.2f\n" $h_video)
+        echo "Spent too much time watching videos ($h_video_rounded) vs working on prof tasks ($h_prof_rounded)" | telegram-send --stdin
+    fi
+    if too_much_break && currently_tracking_tag "break"
+    then
+        echo "Too much break time!" | telegram-send --stdin
     fi
     sleep 10
 done
