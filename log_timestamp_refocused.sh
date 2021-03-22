e6ecd8f08d9f1b1be7c31ffcb4bfd1c981754bd4
Tom Dörr <tom.doerr@tum.de>
2021-03-22 23:58:41 +0100

    Add med tag feature

--- log_timestamp_refocused.sh
+++ log_timestamp_refocused.sh
@@ -1,8 +1,16 @@
-#!/bin/bash
+#!/bin/bash -i
 
-timestamp=$(date +%s)
-cd ~/git/refocused_timestamps_log
-git pull
-echo $timestamp >> log
-git commit -am "Auto commit"
-git push
+set -x 
+
+if [[ $(timew) =~ ' med' ]]
+then
+    rt med
+else
+    at med
+    timestamp=$(date +%s)
+    cd ~/git/refocused_timestamps_log
+    git pull
+    echo $timestamp >> log
+    git commit -am "Auto commit"
+    git push
+fi
