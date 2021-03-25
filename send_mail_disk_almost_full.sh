de93887a0044a956e3a5a7492975ef5d85e3c7cb
Tom Dörr <tom.doerr@tum.de>
2021-03-25 15:01:15 +0100

    Add file

---
+++ send_mail_disk_almost_full.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+FREE_DISK_SPACE_THRESHOLD=100000000
+FREE_DISK_SPACE_THRESHOLD=1000000000
+RECEIVER_EMAIL_ADDRESS="tom.doerr@tum.de"
+
+get_free_disk_space() {
+    df_options=$@
+    df $df_options | grep '/$' | awk '{print $4}' 
+}
+
+while true
+do
+    free_disk_space=$(get_free_disk_space)
+    echo "free_disk_space: " "$free_disk_space"
+    if (( $free_disk_space < $FREE_DISK_SPACE_THRESHOLD ))
+    then
+        echo "" | neomutt -s "Only $(get_free_disk_space -h) free on Desktop." $RECEIVER_EMAIL_ADDRESS
+        sleep 12h
+    fi
+    sleep 1
+done
