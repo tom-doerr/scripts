d4274dacdfc7fab686ae80c89c9319d77835104c
Tom Dörr <tom.doerr@tum.de>
2021-06-28 03:15:32 +0200

    Change show command

--- watch_bms_status.sh
+++ watch_bms_status.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-watch --no-title -n 10 eval "bm status | grep -v '+00:00'" 
+#watch --no-title -n 10 eval "bm status | grep -v '+00:00'" 
+watch --no-title -n 10 eval "~/git/scripts/show_obj_rest_beeminder.sh"
