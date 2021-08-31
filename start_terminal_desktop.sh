497d9df1d56442b2a7abf3e046710a0920ce3631
Tom Dörr <tom.doerr@tum.de>
2021-08-31 16:27:06 +0200

    Remove single instance option

--- start_terminal_desktop.sh
+++ start_terminal_desktop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-if [[ $(uname --nodename) == 'tom-Desktop-18' ]] || [[ $(uname --nodename) == 'desktop-20' ]] || [[ $(uname --nodename) == 'desktop-20-3' ]]; then kitty --single-instance zsh; else kitty --single-instance ssh -X -t gard zsh --login; fi 
+if [[ $(uname --nodename) == 'tom-Desktop-18' ]] || [[ $(uname --nodename) == 'desktop-20' ]] || [[ $(uname --nodename) == 'desktop-20-3' ]]; then kitty zsh; else kitty ssh -X -t gard zsh --login; fi 
