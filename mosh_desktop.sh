b1de16dfac58c19c3b6075bdc4f8145380baf61a
Tom Dörr <tomdoerr96@gmail.com>
2023-01-18 23:22:56 +0100

    Update file

--- mosh_desktop.sh
+++ mosh_desktop.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
- mosh --ssh='ssh -p 39836' -p 60099 abcd.airdns.org
+mosh --ssh='ssh -p 39836' -p 60099 abcd.airdns.org
+#mosh --ssh='ssh -p 2000' -p 60000 desk
+#mosh --ssh='ssh -p 2000' -p 60099 v220220641114192096.goodsrv.de  
+#mosh --ssh='ssh -p 2000' tom@v220220641114192096.goodsrv.de  
