0e5964cdb45f3ac30d2410721f1d40f831459a92
Tom Dörr <tomdoerr96@gmail.com>
2023-07-29 23:41:19 +0200

    Update scripts

--- connect_mirror.sh
+++ connect_mirror.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
- xrandr --output HDMI-1 --mode 2560x1440 --output eDP-1 --mode 1920x1200 --scale-from 2560x1440
+ xrandr --output DP-2 --mode 2560x1440 --output eDP-1 --mode 1920x1200 --scale-from 2560x1440
--- disconnect_mirror.sh
+++ disconnect_mirror.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-xrandr --output HDMI-1 --off --output eDP-1 --mode 1920x1200 --scale-from 1920x1200
+xrandr --output DP-2 --off --output eDP-1 --mode 1920x1200 --scale-from 1920x1200
