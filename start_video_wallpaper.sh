d2d7964d09e7decccb207e18c9189161a9d55502
Tom Dörr <tomdoerr96@gmail.com>
2024-02-05 02:09:54 +0100

    Add file

---
+++ start_video_wallpaper.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+xwinwrap -ov -g 1920x1200+0+0 -- mpv -wid WID ~/Videos/'8H A Beautiful Day in Nature [k-ZXEDMEaew].webm' --no-osc --no-osd-bar --loop-file --player-operation-mode=cplayer --no-audio --panscan=1.0 --no-input-default-bindings &
