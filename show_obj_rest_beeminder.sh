94684628e3c8ca8a8e42e8f9bb48755c8d7a39b1
Tom Dörr <tomdoerr96@gmail.com>
2024-08-04 16:23:40 +0200

    fix: Improve date parsing and time difference calculation in get_diff_times function

--- show_obj_rest_beeminder.sh
+++ show_obj_rest_beeminder.sh
@@ -3,21 +3,15 @@
 get_diff_times() {
     string1="$1"
     string2="$2"
-    if [[ "$string1" == "days" || "$string2" == "days" ]]; then
-        echo "N/A"
-        return
-    fi
-    StartDate=$(date -u -d "$string1" +"%s" 2>/dev/null)
-    FinalDate=$(date -u -d "$string2" +"%s" 2>/dev/null)
-    if [[ -z "$StartDate" || -z "$FinalDate" ]]; then
-        echo "Invalid"
-        return
-    fi
-    if (( StartDate < FinalDate )); then
+    StartDate=$(date -u -d "$string1" +"%s")
+    FinalDate=$(date -u -d "$string2" +"%s")
+    if (( $StartDate < $FinalDate ))
+    then
         date -u -d "0 $FinalDate sec - $StartDate sec" +"%H:%M:%S"
     else
         echo "-"$(date -u -d "0 $StartDate sec - $FinalDate sec" +"%H:%M:%S")
     fi
+
 }
 
 
@@ -27,26 +21,33 @@ get_diff_times() {
 
 #get_diff_times 01:00:00 02:03:00
 
-echo "----------------------------------------"
 echo
-while read -r line; do
-    if [[ $line =~ user ]]; then
-        echo "$line"
+time1=""
+for word in  $(bm status 2>/dev/null | grep -E '(obj|main|ai3)' | sort | awk '{print "  "$2" "$NF" "$1}')
+do
+    if [[ $word =~ user ]]
+    then
+        echo $word
         continue
     fi
-    
-    read goal time1 time2 time3 value rest <<< $(echo "$line" | awk '{print $1, $2, $3, $4, $5, $6, $7, $8, $9}')
-    time1=${time1/+/}
-    time2=${time2/+/}
-    time3=${time3/+/}
-    
-    time_diff1=$(get_diff_times "$time1" "$time2")
-    time_diff2=$(get_diff_times "$time2" "$time3")
-    
-    printf "%-20s %6s %6s %6s %8s %8s %s %s\n" "$goal" "+$time1" "+$time2" "+$time3" "$time_diff1" "$time_diff2" "$value" "$rest"
-done < <(bm status 2>/dev/null | grep -E '(obj|main|ai3|uni)' | sort)
-echo
-echo "----------------------------------------"
+    word=${word/+/}
+    if [[ $time1 == "" ]]
+    then
+        #echo set
+        time1="$word"
+    else
+        #echo calc
+        if [[ $time1 == "✔" ]]
+        then
+            printf "  - - - "
+        else
+            time_diff=$(get_diff_times "$time1:00" "$word:00")
+            printf "  ${time_diff::-3}  $time1  $word  "
+        fi
+        unset time1
+    fi
+done
+bm status 2>/dev/null
 
 
 
@@ -57,8 +58,8 @@ echo "----------------------------------------"
 #
 #
 #
-#for line in  $(bm status | grep obj | sort | awk '{print "  "$2" "$NF" "$1}') 
-#do
+#for line in  $(bm status | grep obj | sort | awk '{print "  "$2" "$NF" "$1}')                                                                                                                                                                                                    
+#do                                                                                                                                                                                                                                                                               
 #    echo $line                                                                                                                                                                                                                                                                   
 #done                                                                                                                                                                                                                                                                             
                                                                                                                                                                                                                                                                                   
@@ -67,4 +68,4 @@ echo "----------------------------------------"
                                                                                                                                                                                                                                                                                   
 #echo                                                                                                                                                                                                                                                                             
 #echo "$(bm status | grep obj | sort | awk '{print "  "$2" "$NF" "$1}')                                                                                                                                                                                                           
-#$(bm status)"                                                                                                                                                                                                                                                                    
+#$(bm status)"   
