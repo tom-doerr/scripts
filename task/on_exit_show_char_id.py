e2aa436f74246b64ee2c4f66f77e3c43f5c61622
Tom Dörr <tomdoerr96@gmail.com>
2025-01-23 15:53:32 +0100

    refactor: Add debug prints to on_exit_show_char_id.py

--- task/on_exit_show_char_id.py
+++ task/on_exit_show_char_id.py
@@ -5,13 +5,18 @@ import sys
 import os
 from task_id_mapper import number_to_char_id, get_start_num
 
+
+print('inside hook')
 # Only show char ID for add commands 
+print(f'Command: {sys.argv}')
 if len(sys.argv) > 1 and sys.argv[1] == 'add':
+    print('inside hook add')
     # Read the task JSON from stdin
     tasks = [json.loads(line) for line in sys.stdin]
     
     # Check if we have any added tasks
     for task in tasks:
+        print('inside hook add task')
         if 'id' in task:
             task_id = task['id']
             char_id = number_to_char_id(task_id)
